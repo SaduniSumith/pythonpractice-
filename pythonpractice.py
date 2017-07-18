1b639bb1e2061dc8089a276a3dc5312b2c72f9d9
SaduniSumith <sadunisumith@gmail.com>
2017-07-18 15:38:43 -0400

    Add files via upload

---
+++ pythonpractice.py
@@ -0,0 +1,161 @@
+#number = 5 #assigning number variable to the value of 5
+
+"""addition = 5 + 7
+subtraction = 400 - 60
+multiplication = 4 * 4
+division = 16 / 4
+
+print (addition)
+print (subtraction)
+print (multiplication)
+print (division)""" 
+
+
+"""exponent = 2**3
+modulo = 15%4 #output of 3"""
+
+"""Costs: $50 
+tax: 13%
+tip: 15%
+cost = 50
+tip = 0.15
+tax = 0.13 
+
+totalwithtax = 50 + (50 * 0.13)
+total = totalwithtax + (50 * 0.15)
+
+print(total)"""#applying basic operations in python
+
+
+"""name = "Bob"
+print (name[1])"""
+
+"""fruit = "apple" 
+print (len(fruit))
+
+
+fruit = "APPLE"
+print (fruit.lower())
+
+fruit = "apple"
+print (fruit.upper())"""
+
+#print ("red " + "hat")
+
+
+""" name = input ("What's your name? ")
+print (name) """
+
+""" number1 = 5
+number2 = 6
+number1 == number2 #equals to
+number1 != nubmer2 #no equal to
+number1 > number2 #is more than
+number1 < number2 #is less than
+number1 <= number2 # is less than or equal to
+number1 >= number2 # is greater than or equal to  """
+
+""" value1 = (20-10) > 15 
+value2 = (10+17) == 3**16 
+value3 = 1**2 <= -1 
+value4 = 40*4 >= -4 
+value5 = 100 != 10*2
+
+Value1 = False
+value2 = False
+value3 = False
+value4 = True
+value5 = False """
+
+
+
+#Boolean opperators: and or not
+
+#and 
+""" value1 = -(-(-(-2))) == -2 and (4 >= 16**0.)
+value2 = 19%4 != 300/10/10 and False
+value3 = - (1**2)<2**0 and 10%10 <= 20-10*2
+
+value1 = False
+value2 = False 
+value3 = True """
+
+#or
+""" value1 = 2**3 == 108%100 or "Apple" == "Orange"
+value2 = 100**0.5 >= 50 or False
+value3 = 1**100 == 100**1 or 3*2*1 != 3+2+1
+
+value1 = True
+value2 = False 
+value3 = False """
+
+
+#not
+""" value1 =  not 3**4 < 4**3
+value2 = not 10%3 <= 10%2
+value3 = not not False 
+
+value1 = True
+value2 = True
+value3 = False """
+
+"""name = "Bob" 
+
+if 7 < 7:
+    print ("Hello")
+elif name == "Bob":
+    print ("My name is Bob") 
+else:
+    print ("Bye") """
+
+
+#and, or, not
+# use a cpmparaor
+#print true
+
+"""name = "John" 
+if 10+7 == 64:
+    print ("what")
+elif name == "John":
+    print ("True")
+else:
+    print ("nope")"""
+    
+A = input ("Type a Number")
+B = input ("Type another number")
+
+if A == B:
+    print (3 * A)
+else:
+    print (A + B)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
